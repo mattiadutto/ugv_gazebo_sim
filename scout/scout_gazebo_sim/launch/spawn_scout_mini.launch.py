309a7f089e0aa4ada734f20d1d8f4b9c12cb111f
mattiadutto <s287598@studenti.polito.it>
2024-02-18 16:54:52 +0100

    Removed empty world, added twist-mux, fix sdf

--- scout/scout_gazebo_sim/launch/spawn_scout_mini.launch.py
+++ scout/scout_gazebo_sim/launch/spawn_scout_mini.launch.py
@@ -13,56 +13,65 @@ from nav2_common.launch import ReplaceString
 def generate_launch_description():
     # Get the urdf file
     sdf_path = os.path.join(
-        get_package_share_directory('scout_gazebo_sim'),
-        'models',
-        'scout_mini',
-        'scout_mini.sdf'
+        get_package_share_directory("scout_gazebo_sim"),
+        "models",
+        "scout_mini",
+        "scout_mini.sdf",
     )
 
     # Launch configuration variables specific to simulation
-    namespace = LaunchConfiguration('namespace', default='scout_mini')
-    x_pose = LaunchConfiguration('x_pose', default='0.0')
-    y_pose = LaunchConfiguration('y_pose', default='0.0')
-    yaw_pose = LaunchConfiguration('yaw_pose', default='0.0')
+    namespace = LaunchConfiguration("namespace", default="scout_mini")
+    x_pose = LaunchConfiguration("x_pose", default="0.0")
+    y_pose = LaunchConfiguration("y_pose", default="0.0")
+    yaw_pose = LaunchConfiguration("yaw_pose", default="0.0")
 
     # Declare the launch arguments
     declare_namespace_arg = DeclareLaunchArgument(
-        'namespace', default_value=namespace,
-        description='Specify robot namespace')
+        "namespace", default_value=namespace, description="Specify robot namespace"
+    )
 
     declare_x_pose_arg = DeclareLaunchArgument(
-        'x_pose', default_value=x_pose,
-        description='Specify robot x position')
+        "x_pose", default_value=x_pose, description="Specify robot x position"
+    )
 
     declare_y_pose_arg = DeclareLaunchArgument(
-        'y_pose', default_value=y_pose,
-        description='Specify robot y position')
+        "y_pose", default_value=y_pose, description="Specify robot y position"
+    )
 
     declare_yaw_pose_arg = DeclareLaunchArgument(
-        'yaw_pose', default_value=yaw_pose,
-        description='Specify robot yaw angle')
+        "yaw_pose", default_value=yaw_pose, description="Specify robot yaw angle"
+    )
 
     # Add namespace to gazebo model file
     namespaced_sdf_file = ReplaceString(
-        source_file=os.path.join(get_package_share_directory('scout_gazebo_sim'),
-        'models',
-        'scout_mini',
-        'scout_mini.sdf'), 
-        replacements={'/robot_namespace': ('/', namespace)})
+        source_file=os.path.join(
+            get_package_share_directory("scout_gazebo_sim"),
+            "models",
+            "scout_mini",
+            "scout_mini.sdf",
+        ),
+        replacements={"/robot_namespace": ("/", namespace)},
+    )
 
     # Nodes
     start_gazebo_ros_spawner_cmd = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
+        package="gazebo_ros",
+        executable="spawn_entity.py",
         arguments=[
-            '-entity', (namespace, '_robot'),
-            '-file', namespaced_sdf_file,
-            '-x', x_pose,
-            '-y', y_pose,
-            '-z', '0.245',
-            '-Y', yaw_pose
+            "-entity",
+            (namespace, "_robot"),
+            "-file",
+            namespaced_sdf_file,
+            "-x",
+            x_pose,
+            "-y",
+            y_pose,
+            "-z",
+            "0.245",
+            "-Y",
+            yaw_pose,
         ],
-        output='screen',
+        output="screen",
     )
 
     ld = LaunchDescription()
@@ -77,4 +86,3 @@ def generate_launch_description():
     ld.add_action(start_gazebo_ros_spawner_cmd)
 
     return ld
-
